f932736ec40b9cd49142e09be256b2d450fb9e5f
kstambaugh <kyle.w.stambaugh1@gmail.com>
2022-07-19 18:40:01 -0400

    added controllers and models

--- server.js
+++ server.js
@@ -9,6 +9,9 @@ const app = express()
 app.get('/', (req, res) => {
     res.send('Welcome to an Awesome App about Breads!')
 })
+//breads
+const breadsController = require('./controllers/breads_controllers.js')
+app.use('/breads', breadsController)
 
 //listen
 app.listen(PORT, () => {
